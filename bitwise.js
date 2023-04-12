15f5b09d50ad562bfe577fbb0c0102ffa2ffd2bd
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-04-12 13:17:18 -0300

    Create bitwise.js

---
+++ bitwise.js
@@ -0,0 +1,17 @@
+//AND//
+let primeiro_numero=10
+let segundo_numero=11 
+let and = primeiro_numero & segundo_numero
+console.log(and)
+//OR//
+let or = primeiro_numero|segundo_numero
+console.log(or)
+//^//
+let esqueci_nome = primeiro_numero^segundo_numero
+console.log(esqueci_nome)
+//deslocamento para esquerda//    
+let left = primeiro_numero<<
+console.log(left)
+//deslocamento para direita//
+let right = segundo_numero>>
+console.log(right)
