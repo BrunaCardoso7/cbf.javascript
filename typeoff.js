85a6be059ea2f98570cd8fb4daf45acd2ded2af9
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-04-12 14:48:29 -0300

    Create typeoff.js

---
+++ typeoff.js
@@ -0,0 +1,8 @@
+let num1 = 10
+let num2 = "10"
+let num3 = num1===num2
+let num4 = {nome:"bruno"}
+console.log("o valor " + num1 + "é do tipo: " + typeof(num1))
+console.log(typeof(num2))
+console.log(typeof(num3))
+console.log(typeof(num4))
