20527d57796919a85211a8fdda1823ef200bb92d
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-04-12 14:36:46 -0300

    operadores ternários

---
+++ ternários.js
@@ -0,0 +1,15 @@
+let num = 10
+res=(!(num%2) ? 'par' : 'impar')
+console.log(res)
+let res = num%2
+
+if (res==0){
+/   console.log(`este númeero ${res} é par`)
+}else {
+   console.log(`este númeero ${res} é impar`)
+}
+
+let status = "I"
+res = (status =="A" ? "Ativo" : "inativo")
+console.log(res)
+
