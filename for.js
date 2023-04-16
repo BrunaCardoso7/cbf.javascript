d470ddc218805eb47d3179c9e5c416ce9f65dfc0
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-04-15 23:38:38 -0300

    forinof

--- for.js
+++ for.js
@@ -1,3 +1,7 @@
-for (let i=0; i<10; i++){
-    console.log(i + "x 2 = " + i*2)
+for (let i=0; i<20; i++){
+    if (i%2==0){
+        console.log(i + 'é um número par')
+    }else{
+        console.log(i + "é um número impar")
+    }
 }
