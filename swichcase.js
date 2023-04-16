15bac30ecd62ae49ceb61af3353c121c13cf1f97
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-04-15 22:52:40 -0300

    Create swichcase.js

---
+++ swichcase.js
@@ -0,0 +1,14 @@
+let colocacao = 5
+switch (colocacao){
+    case 1:
+        console.log("primeiro lugar")
+        break
+    case 2:
+        console.log('segundo lugar')
+        break
+    case 3:
+        console.log("terceiro lugar")
+        break
+    default:
+        console.log("nenhuma informação foi subida")
+}
