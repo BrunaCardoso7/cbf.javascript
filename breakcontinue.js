6479502c6e6c351b2f585dab42f3cbddcedd952c
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-04-16 23:01:07 -0300

    loops e quebras de looping

---
+++ breakcontinue.js
@@ -0,0 +1,10 @@
+let n = 0
+let max = 100
+let pares = 0
+for(let i=n;i<max; i++){
+    if(i%2!=0){
+        continue
+    }
+    pares++
+}
+console.log('tem ' + pares +' NUMERO PARES')
---
+++ dowhile.js
@@ -0,0 +1,5 @@
+let n = 9
+do{
+    let soma = n+10
+    console.log(soma)
+}while(n<10)
