7f4bb167fe6e633a15381e14c4e66b797bbe7ea2
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-04-24 23:21:00 -0300

    Update parametrosrest.js

--- parametrosrest.js
+++ parametrosrest.js
@@ -6,4 +6,24 @@ function add(...valores){
         console.log(soma)
     }
 }
-add(2,  4, 5, 6)
+
+
+function check(age){
+    if (age < 18){
+        console.log('este conteúdo é para maiores de 18 anos, deseja prosseguir?')
+    }else{
+        console.log('boa experiência')
+    }
+}
+
+const f = function(...valores){
+    let res=0
+    for(c of valores){
+        res+=c
+    }
+    return res
+}
+
+
+const nova_funcao = Function("v1", "v2", "return v1+v2")
+console.log(f(10, 3))
